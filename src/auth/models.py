b960ccf648be634970c0a591a803a1c3c1aca40a
Anil Chitti <anil.chitti@otsi.com>
2025-02-26 17:22:59 +0530

    Initial commit

---
+++ src/auth/models.py
@@ -0,0 +1,29 @@
+from  sqlmodel import SQLModel,Column,Field
+import  sqlalchemy.dialects.postgresql as pg
+from datetime import datetime
+import uuid
+
+class User(SQLModel, table=True):
+    __tablename__ ='users'
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            unique=True,
+            nullable=False,
+            default=uuid.uuid4,
+            # info={"description": "Unique identifier for the user account"},
+        )
+    )
+    username: str
+    first_name: str = Field(nullable=True)
+    last_name: str = Field(nullable=True)
+    is_verified: bool = False
+    email :str 
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+
+    
+    
+    def __repr__(self):
+        return f"User {self.username}"
